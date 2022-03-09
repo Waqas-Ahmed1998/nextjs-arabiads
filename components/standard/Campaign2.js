6ad84a1efb88c68ad0937e21598878d6a8375308
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-03-09 05:35:38 +0500

    added image component in campaign

--- components/standard/Campaign2.js
+++ components/standard/Campaign2.js
@@ -1,4 +1,6 @@
+import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image';
 
 function Campaign2() {
   return (
@@ -23,8 +25,8 @@ function Campaign2() {
       }
       <div className='mt-10 grid grid-cols-1 mx-auto relative px-5 py-10'>
         <div className=' mx-auto w-[100%] flex flex-col-reverse xl:flex-row lg:p-14 text-center xl:text-justify'>
-          <div className='p-5 flex-1 space-y-5'>
-            <h3 className='text-xl md:text-[35px] font-semibold'>
+          <div className='p-5 pr-5  flex-1 space-y-5'>
+            <h3 className='text-xl py-8  md:text-[35px] font-semibold'>
               Start a campaign with us!
             </h3>
             <p className='note'>
@@ -33,15 +35,29 @@ function Campaign2() {
               to one of our committed specialists in order to experience what
               influencer marketing can be like when you work with the finest!
             </p>
-            <button className=' text-white  lg:inline-grid px-4 py-2   md:py-4 bg-[#FEA366]    md:w-60  rounded-full md:text-2xl text-base'>
-              REACH OUT
-            </button>
+            <Link href='/'>
+              <button className=' text-white text-base md:py-4  md:px-6 px-4 py-2 md:inline-grid bg-[linear-gradient(#fdc66e,#ff805e)]   font-semibold  xl:w-60  rounded-full md:text-[20px]'>
+                REACH OUT
+              </button>
+            </Link>
           </div>
           <div className='flex-1 '>
-            <img
-              src='https://influencers.ar-ad.com/wp-content/uploads/2021/07/camgaign-img.png'
+            {
+              // <img
+              //   src='https://influencers.ar-ad.com/wp-content/uploads/2021/07/camgaign-img.png'
+              //   alt=''
+              //   className='lg:w-[80%] mx-auto'
+              // />
+            }
+            <Image
+              src='/campaign/pic.png'
               alt=''
-              className='lg:w-[80%] mx-auto'
+              title=''
+              width='500'
+              height='500'
+              // layout='responsive'
+              priority
+              objectFit='contain'
             />
           </div>
         </div>
